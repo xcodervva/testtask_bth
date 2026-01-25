c13f4078008720bc671c1ee2a8812e3cf339260e
xcoder <onlyforwith@gmail.com>
2026-01-25 14:27:20 +0000

    Fix models and seeders

--- src/README.md
+++ src/README.md
@@ -41,6 +41,16 @@ docker compose exec app php artisan migrate
 docker compose exec app php artisan make:model Category -m
 ```
 
+## Создаём модель и миграцию для Product:
+```bash
+docker compose exec app php artisan make:model Product -m
+```
+
+## Запускаем сиды:
+```bash
+docker compose exec app php artisan db:seed
+```
+
 <p align="center"><a href="https://laravel.com" target="_blank"><img src="https://raw.githubusercontent.com/laravel/art/master/logo-lockup/5%20SVG/2%20CMYK/1%20Full%20Color/laravel-logolockup-cmyk-red.svg" width="400" alt="Laravel Logo"></a></p>
 
 <p align="center">
--- src/database/seeders/DatabaseSeeder.php
+++ src/database/seeders/DatabaseSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -15,11 +16,15 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
+        User::updateOrCreate(
+            ['email' => 'admin@example.com'],
+            [
+                'name' => 'Admin',
+                'password' => Hash::make('password'),
+            ]
+        );
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+        $this->call(\Database\Seeders\CategorySeeder::class);
+        $this->call(\Database\Seeders\ProductSeeder::class);
     }
 }
