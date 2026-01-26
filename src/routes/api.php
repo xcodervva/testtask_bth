3e07c01415601eef05173807d3930c3bfa674ba8
xcoder <onlyforwith@gmail.com>
2026-01-26 17:47:55 +0000

    Finalize api routes for public and admin products

--- src/routes/api.php
+++ src/routes/api.php
@@ -12,3 +12,10 @@ Route::middleware('auth:sanctum')->get('/me', function (Request $request) {
 Route::get('/categories', [CategoryController::class, 'index']);
 Route::get('/products', [ProductController::class, 'index']);
 Route::get('/products/{id}', [ProductController::class, 'show']);
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::post('/products', [ProductController::class, 'store']);
+    Route::put('/products/{id}', [ProductController::class, 'update']);
+    Route::patch('/products/{id}', [ProductController::class, 'update']);
+    Route::delete('/products/{id}', [ProductController::class, 'destroy']);
+});
