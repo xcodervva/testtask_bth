0449306345a0f195b6ba87d646e16fb99904b9ae
xcoder <onlyforwith@gmail.com>
2026-01-27 12:24:37 +0000

    Add Pinia to frontend

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,8 +1,9 @@
 import { createApp } from 'vue';
+import { createPinia } from "pinia";
 import ElementPlus from 'element-plus';
 import 'element-plus/dist/index.css';
 
 import './style.css';
 import App from './App.vue';
 
-createApp(App).use(ElementPlus).mount('#app');
+createApp(App).use(createPinia()).use(ElementPlus).mount('#app');
