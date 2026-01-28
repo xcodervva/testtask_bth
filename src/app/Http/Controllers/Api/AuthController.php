bb4b1c0b59d402891f0618309e26c638c37987ac
xcoder <onlyforwith@gmail.com>
2026-01-28 19:12:21 +0300

    Create AuthController with login function

---
+++ src/app/Http/Controllers/Api/AuthController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class AuthController extends Controller
+{
+    public function login(Request $request)
+    {
+        $credentials = $request->validate([
+            'email' => ['required', 'email'],
+            'password' => ['required'],
+        ]);
+
+        if (!Auth::attempt($credentials)) {
+            return response()->json([
+                'message' => 'Invalid credentials',
+            ], 401);
+        }
+
+        $user = $request->user();
+
+        $token = $user->createToken('admin-token')->plainTextToken;
+
+        return response()->json([
+            'token' => $token,
+        ]);
+    }
+}
--- src/routes/api.php
+++ src/routes/api.php
@@ -1,9 +1,12 @@
 <?php
 
+use App\Http\Controllers\Api\AuthController;
 use App\Http\Controllers\Api\CategoryController;
 use App\Http\Controllers\Api\ProductController;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Http\Request;
+
+Route::post('/login', [AuthController::class, 'login']);
 
 Route::middleware('auth:sanctum')->get('/me', function (Request $request) {
     return $request->user();
